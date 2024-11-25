64c061812f77a416fceed65487d767c7c4c33896
Manuel Rodriguez <marobej@gmail.com>
2024-11-25 14:32:53 -0800

    Refactored Leases to work with new layout

--- source/frontend/src/features/leases/detail/LeasePages/checklist/LeaseChecklistContainer.tsx
+++ source/frontend/src/features/leases/detail/LeasePages/checklist/LeaseChecklistContainer.tsx
@@ -1,46 +1,44 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
-import LoadingBackdrop from '@/components/common/LoadingBackdrop';
 import { Claims } from '@/constants';
 import * as API from '@/constants/API';
 import { TabInteractiveContainerProps } from '@/features/mapSideBar/shared/TabDetail';
 import { IChecklistViewProps } from '@/features/mapSideBar/shared/tabs/checklist/detail/ChecklistView';
 import { useLeaseRepository } from '@/hooks/repositories/useLeaseRepository';
 import useKeycloakWrapper from '@/hooks/useKeycloakWrapper';
+import useTraceUpdate from '@/hooks/util/useTraceUpdate';
 import { ApiGen_Concepts_FileChecklistItem } from '@/models/api/generated/ApiGen_Concepts_FileChecklistItem';
 import { exists } from '@/utils';
 
 const LeaseChecklistContainer: React.FunctionComponent<
   React.PropsWithChildren<TabInteractiveContainerProps<IChecklistViewProps>>
-> = ({ fileId, pathResolverHook, View }) => {
-  debugger;
+> = props => {
+  useTraceUpdate(props);
   const [checklist, setChecklist] = useState<ApiGen_Concepts_FileChecklistItem[]>([]);
   const keycloak = useKeycloakWrapper();
-  const pathResolver = pathResolverHook();
+  const pathResolver = props.pathResolverHook();
 
   const showEditButton = keycloak.hasClaim(Claims.LEASE_EDIT);
 
   const { getLeaseChecklist } = useLeaseRepository();
+  const getLease = getLeaseChecklist.execute;
 
   const fetchChecklist = useCallback(async () => {
-    debugger;
-    const result = await getLeaseChecklist.execute(fileId);
+    const result = await getLease(props.fileId);
     if (exists(result)) {
       setChecklist(result);
     }
-  }, [fileId, getLeaseChecklist]);
+  }, [props.fileId, getLease]);
 
   useEffect(() => {
     fetchChecklist();
   }, [fetchChecklist]);
 
   const onEdit = () => {
-    pathResolver.editDetails('lease', fileId, 'checklist');
+    pathResolver.editDetails('lease', props.fileId, 'checklist');
   };
 
-  if (getLeaseChecklist.loading) {
-    <LoadingBackdrop />;
-  }
+  const View = props.View;
 
   return (
     <View
